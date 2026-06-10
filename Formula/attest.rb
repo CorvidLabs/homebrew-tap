f942ea38b80b8a67c89ee1f55a4edd8052c7615d
0xLeif <leif.algo@pm.me>
2026-06-10 12:31:55 -0600

    Update: attest 0.3.0 (macOS and Linux)

--- Formula/attest.rb
+++ Formula/attest.rb
@@ -2,11 +2,11 @@ class Attest < Formula
   desc "Signed provenance for code changes, a verifiable ledger keyed to commit SHAs"
   homepage "https://github.com/CorvidLabs/attest"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
-    url "https://github.com/CorvidLabs/attest/releases/download/v0.2.0/attest-macos-universal"
-    sha256 "340409d992e2dac20f15c99c6652bc849bf0a7a62bd3d8c552c04637e00e6e8e"
+    url "https://github.com/CorvidLabs/attest/releases/download/v0.3.0/attest-macos-universal"
+    sha256 "ee52c4751afb96899213f6a5cf6a4fd741403e9d419829cf01d4b67e618a0a63"
 
     def install
       bin.install "attest-macos-universal" => "attest"
@@ -14,6 +14,6 @@ class Attest < Formula
   end
 
   test do
-    assert_match "0.2.0", shell_output("#{bin}/attest --version")
+    assert_match "0.3.0", shell_output("#{bin}/attest --version")
   end
 end
