ae680ec9b79add555ad04d2205d2d69e8ae47b71
0xLeif <leif.algo@pm.me>
2026-06-10 08:57:07 -0600

    Update: augur 0.3.0 (macOS and Linux)

--- Formula/augur.rb
+++ Formula/augur.rb
@@ -2,11 +2,11 @@ class Augur < Formula
   desc "Graded trust for code changes: risk scoring (proceed/review/block) for humans and agents"
   homepage "https://github.com/CorvidLabs/augur"
   license "MIT"
-  version "0.2.1"
+  version "0.3.0"
 
   on_macos do
-    url "https://github.com/CorvidLabs/augur/releases/download/v0.2.1/augur-macos-universal"
-    sha256 "60cc7f70225dd5c4c3d6ac0dec0c4175725049d6706a111dbd45a14996c2c5b4"
+    url "https://github.com/CorvidLabs/augur/releases/download/v0.3.0/augur-macos-universal"
+    sha256 "78124d36e1e100dbcff0ecd45d49d6ebcf1f6818630b96ae0f57a29ee4d3ba62"
 
     def install
       bin.install "augur-macos-universal" => "augur"
@@ -14,6 +14,6 @@ class Augur < Formula
   end
 
   test do
-    assert_match "0.2.1", shell_output("#{bin}/augur --version")
+    assert_match "0.3.0", shell_output("#{bin}/augur --version")
   end
 end
