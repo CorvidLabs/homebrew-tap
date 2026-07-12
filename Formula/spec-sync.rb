f6c61f2b341ebd9730a704390698712510f42833
0xLeif <leif.algo@pm.me>
2026-07-11 18:36:06 -0600

    chore: update spec-sync to 5.0.1

--- Formula/spec-sync.rb
+++ Formula/spec-sync.rb
@@ -1,30 +1,30 @@
 class SpecSync < Formula
   desc "Bidirectional spec-to-code validation for CI-enforced contracts"
   homepage "https://corvidlabs.xyz/spec-sync"
-  version "5.0.0"
+  version "5.0.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/CorvidLabs/spec-sync/releases/download/v#{version}/specsync-macos-aarch64.tar.gz"
-      sha256 "a92151f09ce35a209f35bd4a76420e8cd722630c56edcc51fc0463ae5657f5ff"
+      sha256 "5ebadc47c1b6b6d409af1b09d88d1ebc945cb11a32789f9e7b4785acfcc5246c"
     end
 
     on_intel do
       url "https://github.com/CorvidLabs/spec-sync/releases/download/v#{version}/specsync-macos-x86_64.tar.gz"
-      sha256 "6005134ce9280177b13f57157862c9ec2114bfdf5d8d2f3c927da5ed6342c60b"
+      sha256 "994ab5aacc1045e704bb198c1495d737149e67e8ae11af22608d8357aaaba05d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/CorvidLabs/spec-sync/releases/download/v#{version}/specsync-linux-aarch64.tar.gz"
-      sha256 "9b785202b1661c130b641856a1ef09e41383e59cf1884f909dfab101b8cb9a58"
+      sha256 "9077d06364ae12647c2b77ff46066f9500ad7d73e02c662571d575c25205fe49"
     end
 
     on_intel do
       url "https://github.com/CorvidLabs/spec-sync/releases/download/v#{version}/specsync-linux-x86_64.tar.gz"
-      sha256 "2f1659d54024ff5546e6fda2b64c5a14145c546e8c4f3a9419eeb85ac19dcd77"
+      sha256 "05e111dbcc042a2d2d1609e767ffdce2b5feb1a37f32e7b625567fcb176462e9"
     end
   end
 
@@ -35,6 +35,6 @@ class SpecSync < Formula
   end
 
   test do
-    assert_match "5.0.0", shell_output("#{bin}/specsync --version")
+    assert_match "5.0.1", shell_output("#{bin}/specsync --version")
   end
 end
